679eb5d10e76007661884e65a46ad14b5d2b284d
Fabien Garcia <fab0670312047@gmail.com>
2013-05-06 11:36:55 +0200

    First rail application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabcook::Application.config.secret_token = '8a751ccfe9d01749d2e18d891c8fd3619a7f8b6de13cc2f5a011ab33a94b07407b81568e95403b240b0e82944dff9d7f97fbf4c8c02f76517d9aced56d5b197c'
