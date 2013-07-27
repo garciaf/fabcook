a8160a61c52d38153e2c128514d037b1b2b25576
Fabien Garcia <fab0670312047@gmail.com>
2013-07-27 19:54:35 +0200

    add vagrant config

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,10 @@ role :app, "192.168.33.10"                          # This may be the same as yo
 role :db,  "192.168.33.10", :primary => true # This is where Rails migrations will run
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
+set :deploy_to, "/home/vagrant/rails"
+set :rails_env, "production"
+set :branch, "master"
+set :user, "vagrant"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
