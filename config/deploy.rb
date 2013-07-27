cf5f51e51333225f12ad80b7707b845ef1a5247e
Fabien Garcia <fab0670312047@gmail.com>
2013-07-27 19:49:14 +0200

    add config

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,10 +10,9 @@ set :scm_passphrase, "vagrant"
 
 set :ssh_options, { :forward_agent => true }
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "192.168.33.10"                          # Your HTTP server, Apache/etc
+role :app, "192.168.33.10"                          # This may be the same as your `Web` server
+role :db,  "192.168.33.10", :primary => true # This is where Rails migrations will run
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
